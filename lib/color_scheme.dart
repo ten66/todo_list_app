b1feab4e1881e1f5fc24ff49d7d5b9ebeb2bcb8e
ten66 <ikesho.1217@gmail.com>
2023-12-05 23:28:49 +0900

    #3 colorSchemeをファイル化し、main.dartに変更を反映

---
+++ lib/color_scheme.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+const greenColorScheme = ColorScheme(
+  brightness: Brightness.light,
+  primary: Color(0xFFAEC3AE),
+  onPrimary: Color(0xFF303030),
+  secondary: Color(0xFF94A684),
+  onSecondary: Color(0xFF303030),
+  error: Colors.red,
+  onError: Colors.white,
+  background: Color(0xFFE4E4D0),
+  onBackground: Color(0xFF303030),
+  surface: Color(0xFFFDE4ED),
+  onSurface: Color(0xFF303030),
+);
+
+const purpleColorScheme = ColorScheme(
+  brightness: Brightness.dark,
+  primary: Color(0xFF543295),
+  onPrimary: Color(0xFFE2E2E2),
+  secondary: Color(0xFFB086D6),
+  onSecondary: Color(0xFFE2E2E2),
+  error: Colors.red,
+  onError: Colors.white,
+  background: Colors.black,
+  onBackground: Color(0xFFE2E2E2),
+  surface: Color(0xFFFDE4ED),
+  onSurface: Color(0xFFE2E2E2),
+);
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:todo_list_app/color_scheme.dart';
 import 'package:todo_list_app/pages/home_page.dart';
 
 void main() async {
@@ -21,32 +22,8 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // colorScheme: const ColorScheme(
-        //   brightness: Brightness.light,
-        //   primary: Color(0xFFAEC3AE),
-        //   onPrimary: Color(0xFF303030),
-        //   secondary: Color(0xFF94A684),
-        //   onSecondary: Color(0xFF303030),
-        //   error: Colors.red,
-        //   onError: Colors.white,
-        //   background: Color(0xFFE4E4D0),
-        //   onBackground: Color(0xFF303030),
-        //   surface: Color(0xFFFDE4ED),
-        //   onSurface: Color(0xFF303030),
-        // ),
-        colorScheme: const ColorScheme(
-          brightness: Brightness.dark,
-          primary: Color(0xFF543295),
-          onPrimary: Color(0xFFE2E2E2),
-          secondary: Color(0xFFB086D6),
-          onSecondary: Color(0xFFE2E2E2),
-          error: Colors.red,
-          onError: Colors.white,
-          background: Colors.black,
-          onBackground: Color(0xFFE2E2E2),
-          surface: Color(0xFFFDE4ED),
-          onSurface: Color(0xFFE2E2E2),
-        ),
+        colorScheme: greenColorScheme,
+        // colorScheme: purpleColorScheme,
         useMaterial3: true,
       ),
       home: const HomePage(),
